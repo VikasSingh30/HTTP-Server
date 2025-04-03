a98584c79fe3636a70ef973b55743c85bff73bf4
VikasSingh30 <singhvikas.sv30@gmail.com>
2025-04-03 11:39:30 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const net = require("net");
+// TCP
+// CRLF
 
 
 console.log("Logs from your program will appear here!");
@@ -6,7 +8,9 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
+    socket.closed();
   });
 });
 
