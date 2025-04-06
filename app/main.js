559f235a507576077d5af09c43dd1160860ca6e7
VikasSingh30 <singhvikas.sv30@gmail.com>
2025-04-06 16:35:43 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -5,8 +5,8 @@ console.log("Logs from your program will appear here!");
 
 
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
   });
 });
